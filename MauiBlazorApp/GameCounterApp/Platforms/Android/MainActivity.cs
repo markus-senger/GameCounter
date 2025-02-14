46c6d69414f11ba6a0eb66407f8045509eb32cdf
markus-senger <senger1995@gmail.com>
2025-02-14 18:14:03 +0100

    add maui blazor app

---
+++ MauiBlazorApp/GameCounterApp/Platforms/Android/MainActivity.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace GameCounterApp
+{
+    [Activity(
+    //Theme = "@style/Maui.SplashTheme",
+    Theme = "@style/Maui.MainTheme.NoActionBar",
+    MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
+}
